a8f4fc28d8c14ff05fa9f580b0f7c4315c1921ab
Mathesh M <mathesh4193@gmail.com>
2025-10-09 15:57:11 +0530

    Commit

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,28 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: true, // 465 for Gmail SSL
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
 
-  await transporter.sendMail({
-    from: `"Password Reset" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+    const info = await transporter.sendMail({
+      from: `"${process.env.FROM_NAME}" <${process.env.FROM_EMAIL}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("Email sent:", info.response);
+  } catch (error) {
+    console.error(" Email error:", error.message);
+  }
 };
 
 module.exports = sendEmail;
