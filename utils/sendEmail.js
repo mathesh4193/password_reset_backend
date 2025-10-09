4fc1789afa81355ca89558ea56cc1f92f3183127
Mathesh M <mathesh4193@gmail.com>
2025-10-09 15:21:17 +0530

    Commit

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,13 +1,10 @@
 const nodemailer = require('nodemailer');
 
 async function sendEmail({ to, subject, text, html }) {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail', // Use Gmail service directly
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS
-    }
-  });
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS }
+});
 
   const info = await transporter.sendMail({
     from: process.env.FROM_EMAIL || process.env.SMTP_USER,
